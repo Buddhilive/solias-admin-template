04764a6acac3e008284e91697e25e79365e4870d
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-24 17:20:29 +0530

    Add sidenav bahavior

--- src/app/layout/side-nav/side-nav.component.ts
+++ src/app/layout/side-nav/side-nav.component.ts
@@ -1,20 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   template: `
-    <div class="sidenav">
-      <button class="sidenav__toggle">
+    <div class="sidenav" [ngClass]="{ 'sidenav--expand': !isCollapsed }">
+      <button class="sidenav__toggle" (click)="isCollapsed = !isCollapsed">
         <span class="material-icons">menu</span>
       </button>
-      <div class="sidenav__content">
-      </div>
+      <div class="sidenav__content"></div>
     </div>
   `,
-  styleUrl: './side-nav.component.scss'
+  styleUrl: './side-nav.component.scss',
 })
 export class SideNavComponent {
-
+  isCollapsed = true;
 }
